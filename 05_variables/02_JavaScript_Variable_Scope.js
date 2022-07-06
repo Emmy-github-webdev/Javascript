15127005ec50dff3eef09ce861b771d61f8ff428
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-07 00:03:39 +0100

    Javascript variable scope

---
+++ 05_variables/02_JavaScript_Variable_Scope.js
@@ -0,0 +1,6 @@
+/**
+ * JavaScript Variable Scope
+  The scope of a variable is the region the variable is defined within your program. In Javascript we have
+  1. Global Variable which has global scope which means it can be defined anywhere within the javascript code.
+  2. Local variable which has local scope which means that it can only be visible within the function it is defined.
+ */
