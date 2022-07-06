f5623722675d91d6627c1526e5ff2481bfa5a065
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-07-06 23:25:39 +0100

    Javascript datatype

---
+++ 05_variables/01_JavaScript_Datatypes.js
@@ -0,0 +1,13 @@
+/* 
+Javascript allows you to work with
+
+1. primitive datatype
+  a. Number
+  b. String
+  c. Boolean
+2. Trivial datatype
+  a. Null
+  b. Undefined
+3. Composite DataType
+  a. Object
+*/
